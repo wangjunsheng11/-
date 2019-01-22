164a5b1460a3f1bfe915d6c768f4c30a6c85b985
wangwei <515002954@qq.com>
2019-01-22 17:56:47 +0800

    更新：     1.更新token在Header中获取的方式

--- src/main/java/com/kakacl/product_service/filter/LoginValidateFilter.java
+++ src/main/java/com/kakacl/product_service/filter/LoginValidateFilter.java
@@ -1,22 +1,20 @@
 package com.kakacl.product_service.filter;
 
-import com.alibaba.druid.support.json.JSONUtils;
+import com.kakacl.product_service.config.Constants;
 import com.kakacl.product_service.utils.ErrorCode;
 import com.kakacl.product_service.utils.JWTUtils;
-import com.kakacl.product_service.utils.Resp;
 import io.jsonwebtoken.Claims;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 /**
  * @author wangwei
@@ -39,11 +37,22 @@ public class LoginValidateFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String token = ((HttpServletResponse) servletResponse).getHeader("token");
+        String token = "";
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        Enumeration headerNames = request.getHeaderNames();
+        // 遍历Header
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            if("token".equals(key)) {
+                token = value;
+            }
+        }
+
         if(StringUtils.isBlank(token)) {
             token = servletRequest.getParameter("token");
         }
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
@@ -59,7 +68,7 @@ public class LoginValidateFilter implements Filter {
             // 判断是否过期 没有过期在header更新token
             try{
                 Claims claims = JWTUtils.parseJWT(token);
-                ((HttpServletResponse) servletResponse).setHeader("token", JWTUtils.createJWT(claims.getId(), claims.getIssuer(), claims.getSubject(), 1000 * 60 * 30));
+                ((HttpServletResponse) servletResponse).setHeader("token", JWTUtils.createJWT(claims.getId(), claims.getIssuer(), claims.getSubject(), Constants.CONSTANT_1000 * Constants.CONSTANT_60 * Constants.CONSTANT_30));
                 filterChain.doFilter(servletRequest, servletResponse);
             } catch (Exception e) {
                 PrintWriter writer = null;
