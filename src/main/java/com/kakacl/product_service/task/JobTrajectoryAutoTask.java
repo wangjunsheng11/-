65cf62e9f42479e1a71c1991fafd05f1f41e9789
wangwei <515002954@qq.com>
2019-01-26 09:45:05 +0800

    更新：     1.添加求职者轨迹信息收集到周周发

--- src/main/java/com/kakacl/product_service/task/JobTrajectoryAutoTask.java
+++ src/main/java/com/kakacl/product_service/task/JobTrajectoryAutoTask.java
@@ -1,35 +1,37 @@
 package com.kakacl.product_service.task;
 
-import com.alibaba.fastjson.JSON;
 import com.kakacl.product_service.service.JobTrajectoryAutoService;
-import org.apache.commons.lang.time.DateFormatUtils;
-import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 import java.util.*;
 
 /**
  * @author wangwei
  * @version v1.0.0
- * @description 定时任务测试
- * @date 2019-01-25
+ * @description 求职者工作轨迹任务定时器
+ * @date 2019-01-26
  */
 @Component
 @Configurable
 @EnableScheduling
-public class ScheduledTest{
+public class JobTrajectoryAutoTask {
 
-    // 求职轨迹
+    // 求职轨迹服务类
     @Autowired
     private JobTrajectoryAutoService jobTrajectoryAutoService;
 
     /*
+     * 求职者轨迹定时任务
      *
+     * 步骤：
+     *  1. 获取最多50条门店系统中求职者手机号、求职轨迹主键、求职者当时的手机号、开始工作时间、补贴截止时间，条件：周周发轨迹表中没有记录编号no的数据，删除标记为没有删除，截止时间不为空，时间按降序获取。
+     *  2. 根据门店系统求职者当时的手机号码，获取门店系统求职者主键。
+     *  3. 根据门店系统求职者当时的手机号码查询出求职者的状态。
+     *  4. 保存门店系统收集的数据到周周发求职者历史求职轨迹中。
      *
      * 需要将用户的入职信息从表store_employee_history - zzf_user_employee_hitory
      * 获取用户的工资信息 - 补贴信息 - 入职时间 - 离职信息 - 补贴到期时间 等信息
@@ -42,9 +44,8 @@ public class ScheduledTest{
      * store_employee_history - 是否离职，根据工作状态进行判断
      *
      */
-//    @Scheduled(fixedRate = 1000 * 10)
-    @Deprecated
-    public void test_10_s(){
+    @Scheduled(fixedRate = 1000 * 10)
+    public void employeeOrbitTask(){
         Map params = new HashMap();
 //        System.out.println("查询用户的求职轨迹 - " + dateFormat ().format (new Date()));
         params.clear();
@@ -88,7 +89,7 @@ public class ScheduledTest{
                 return;
             } else {
                 String work_status = dataMap.get("work_status").toString();
-                System.out.println(String.format("work_status: %s", work_status));
+//                System.out.println(String.format("work_status: %s", work_status));
                 // 增加信息
                 params.clear();
                 params.put("id", UUID.randomUUID().toString().replaceAll("-", ""));
@@ -106,41 +107,4 @@ public class ScheduledTest{
             }
         }
     }
-
-    /**
-     * 日期格式字符串转换成时间戳
-     * @param date_str 字符串日期
-     * @param format 如：yyyy-MM-dd HH:mm:ss
-     * @return
-     */
-    public static String date2TimeStamp(String date_str,String format){
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat(format);
-            return String.valueOf(sdf.parse(date_str).getTime()/1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
-    /*@Scheduled(fixedRate = 1000 * 30)
-    public void reportCurrentTime(){
-        System.out.println ("Scheduling Tasks Examples: The time is now " + dateFormat ().format (new Date()));
-    }*/
-
-    /*@Scheduled(fixedRate = 1000 * 1)
-    public void a_1_s(){
-        System.out.println ("Scheduling Tasks Examples: The time is now " + dateFormat ().format (new Date()));
-    }*/
-
-    //每1分钟执行一次
-    @Scheduled(cron = "0 */1 *  * * * ")
-    public void reportCurrentByCron(){
-        System.out.println ("Scheduling Tasks Examples By Cron: The time is now " + dateFormat ().format (new Date ()));
-    }
-
-    private SimpleDateFormat dateFormat(){
-        return new SimpleDateFormat ("HH:mm:ss");
-    }
-
 }
