d11cc92b9892dbb71fa97fcba68ca6a1eb09db60
wangwei <515002954@qq.com>
2019-01-26 10:20:59 +0800

    更新：     1.添加求职者轨迹信息收集到周周发

--- src/main/java/com/kakacl/product_service/task/JobTrajectoryAutoTask.java
+++ src/main/java/com/kakacl/product_service/task/JobTrajectoryAutoTask.java
@@ -1,8 +1,12 @@
 package com.kakacl.product_service.task;
 
 import com.kakacl.product_service.service.JobTrajectoryAutoService;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -20,12 +24,19 @@ import java.util.*;
 @EnableScheduling
 public class JobTrajectoryAutoTask {
 
+    public Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     // 求职轨迹服务类
     @Autowired
     private JobTrajectoryAutoService jobTrajectoryAutoService;
 
+    private final static String countKey ="redis:employee_orbit_task_auto_lock";
+
     /*
-     * 求职者轨迹定时任务
+     * 求职者轨迹定时任务-多久执行一次需要根据线上服务器负载和刷新频率， 不建议超过10s.
      *
      * 步骤：
      *  1. 获取最多50条门店系统中求职者手机号、求职轨迹主键、求职者当时的手机号、开始工作时间、补贴截止时间，条件：周周发轨迹表中没有记录编号no的数据，删除标记为没有删除，截止时间不为空，时间按降序获取。
@@ -44,67 +55,90 @@ public class JobTrajectoryAutoTask {
      * store_employee_history - 是否离职，根据工作状态进行判断
      *
      */
-    @Scheduled(fixedRate = 1000 * 10)
+    @Scheduled(fixedRate = 1000 * 5)
     public void employeeOrbitTask(){
-        Map params = new HashMap();
-//        System.out.println("查询用户的求职轨迹 - " + dateFormat ().format (new Date()));
-        params.clear();
-        List<Map> data = jobTrajectoryAutoService.findListo0_50(params);
-        for (int i = 0; i < data.size(); i++) {
-            String company_id = data.get(i).get("company_id").toString();
-            // 求职轨迹主键
-            String orbit_id = data.get(i).get("id").toString();
-            // 求职者手机号
-            String phone = data.get(i).get("party_b_phone_num").toString();
 
-            Date start_wark_date = (Date)(data.get(i).get("start_wark_date"));
-            Date end_wark_date = (Date)(data.get(i).get("end_wark_date"));
-            String no = data.get(i).get("no").toString();
-            // 根据求职者手机号查询的身份证号码
-            params.clear();
-            params.put("phone", phone);
-            Map userinfo = jobTrajectoryAutoService.findUserInfoByPhone(params);
-            if(userinfo == null) {
-                return;
-            };
-//            System.out.println(JSON.toJSONString(userinfo));
-            String zzf_userid = userinfo.get("id").toString();
-//            System.out.println(String.format("zzf_userid : %s", zzf_userid));
-//            System.out.println(String.format("start_wark_date : %s", start_wark_date));
-//            System.out.println(String.format("company_id: %s", company_id));
-//            System.out.println(String.format("no: %s", no));
+        // 加Redis同步锁
+        final String key = String.format("redis:employee_orbit_task_auto_task:id:%s", "employeeOrbitTask");
+        Boolean res = true;
+        while(res) {
+            String value = UUID.randomUUID().toString() + System.nanoTime();
+            res = stringRedisTemplate.opsForValue().setIfAbsent(key, value);
+            if (res) {
+                stringRedisTemplate.opsForValue().increment(countKey, 1L);
+                try {
+                    res = false;
+                    log.info("开始执行业务啦： className: {}", this.getClass().getName());
+                    log.info("开始执行业务啦： FunctionName employeeOrbitTask , value: {}", value);
+                    Map params = new HashMap();
+                    params.clear();
+                    List<Map> data = jobTrajectoryAutoService.findListo0_50(params);
+                    for (int i = 0; i < data.size(); i++) {
+                        String company_id = data.get(i).get("company_id").toString();
+                        // 求职轨迹主键
+                        String orbit_id = data.get(i).get("id").toString();
+                        // 求职者手机号
+                        String phone = data.get(i).get("party_b_phone_num").toString();
 
-            // 当前工作状态-从当前的状态中获取 离职时间
-            // 根据求职者手机号码查询求职者的信息
-            params.clear();
-            params.put("phone", phone);
-            Map dataMap = jobTrajectoryAutoService.findPhoneByUserInfo(params);
-            if(dataMap == null) {
-                return;
-            };
-            params.clear();
-            params.put("phone", phone);
-            dataMap = jobTrajectoryAutoService.findUserBaseInfo(params);
-            if(dataMap == null) {
-                return;
+                        Date start_wark_date = (Date)(data.get(i).get("start_wark_date"));
+                        Date end_wark_date = (Date)(data.get(i).get("end_wark_date"));
+                        String no = data.get(i).get("no").toString();
+                        // 根据求职者手机号查询的身份证号码
+                        params.clear();
+                        params.put("phone", phone);
+                        Map userinfo = jobTrajectoryAutoService.findUserInfoByPhone(params);
+                        if(userinfo == null) {
+                            return;
+                        };
+                        String zzf_userid = userinfo.get("id").toString();
+
+                        // 当前工作状态-从当前的状态中获取 离职时间
+                        // 根据求职者手机号码查询求职者的信息
+                        params.clear();
+                        params.put("phone", phone);
+                        Map dataMap = jobTrajectoryAutoService.findPhoneByUserInfo(params);
+                        if(dataMap == null) {
+                            return;
+                        };
+                        params.clear();
+                        params.put("phone", phone);
+                        dataMap = jobTrajectoryAutoService.findUserBaseInfo(params);
+                        if(dataMap == null) {
+                            return;
+                        } else {
+                            String work_status = dataMap.get("work_status").toString();
+                            // 增加信息
+                            params.clear();
+                            params.put("id", UUID.randomUUID().toString().replaceAll("-", ""));
+                            params.put("no", no);
+                            params.put("user_id", zzf_userid);
+                            params.put("orbit_id", orbit_id);
+                            params.put("company_id", company_id);
+                            params.put("work_status", work_status);
+                            params.put("entry_time", start_wark_date.getTime() / 1000);
+                            params.put("resignation_time", end_wark_date.getTime() / 1000);
+                            params.put("create_time", System.currentTimeMillis() / 1000 + "");
+                            params.put("create_by", zzf_userid);
+                            // 保存用户轨迹数据到周周发
+                            jobTrajectoryAutoService.insertOne(params);
+                        }
+                    }
+                } catch (Exception e) {
+                } finally {
+                    // 释放锁
+                    String redisValue = stringRedisTemplate.opsForValue().get(key);
+                    if (StringUtils.isNotBlank(redisValue) && redisValue.equals(value)) {
+                        stringRedisTemplate.delete(key);
+                    }
+                }
             } else {
-                String work_status = dataMap.get("work_status").toString();
-//                System.out.println(String.format("work_status: %s", work_status));
-                // 增加信息
-                params.clear();
-                params.put("id", UUID.randomUUID().toString().replaceAll("-", ""));
-                params.put("no", no);
-                params.put("user_id", zzf_userid);
-                params.put("orbit_id", orbit_id);
-                params.put("company_id", company_id);
-                params.put("work_status", work_status);
-                params.put("entry_time", start_wark_date.getTime() / 1000);
-                params.put("resignation_time", end_wark_date.getTime() / 1000);
-                params.put("create_time", System.currentTimeMillis() / 1000 + "");
-                params.put("create_by", zzf_userid);
-                // 保存用户轨迹数据到周周发
-                jobTrajectoryAutoService.insertOne(params);
+                res = true;
+                try {
+                    Thread.sleep(1000L);
+                } catch (Exception e) {
+                }
             }
         }
+
     }
 }
